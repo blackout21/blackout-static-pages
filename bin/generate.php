235a99d189739a097c92687cd9a31986e44e7e97
Timon F <dev@timonf.de>
2019-03-18 23:47:34 +0100

    Adds unformal German language; adds wallpaper version

--- bin/generate.php
+++ bin/generate.php
@@ -8,32 +8,39 @@ use Twig\Loader\FilesystemLoader;
 require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 // paths
-$templatesPath = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src';
+$templatesPath = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'templates';
 $translationsPath = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'translations';
 $targetPath = __DIR__ . DIRECTORY_SEPARATOR . '..';
 
 // tools
 $loader = new FilesystemLoader($templatesPath);
 $renderer = new Environment($loader);
-$finder = Finder::create();
+$templates = Finder::create()->files()->in($templatesPath)->name('*.html.twig');
+$translations = Finder::create()->files()->in($translationsPath)->name('*.json');
 
-foreach ($finder->files()->in($translationsPath)->name('*.json') as $file) {
-    /** @var $file SplFileInfo */
-    $language = $file->getBasename('.json');
-    $filename = sprintf('blackout_%s.html', $language);
-    $translationFile = file_get_contents($file->getPathname());
-    $translationData = json_decode($translationFile, true);
-    $translationData['language'] = $language;
+foreach ($templates as $templateFile) {
+    /** @var $templateFile \SplFileInfo */
+    $templateName = $templateFile->getBasename('.html.twig');
+    printf('Processing template "%s"…' . PHP_EOL, $templateFile->getBasename());
 
-    if (json_last_error() !== JSON_ERROR_NONE) {
-        printf('ERROR: %s in language %s' . PHP_EOL, json_last_error_msg(), $language);
-        return 1;
+    foreach ($translations as $file) {
+        /** @var $file \SplFileInfo */
+        $language = $file->getBasename('.json');
+        $filename = sprintf('%s_%s.html', $templateName, $language);
+        $translationFile = file_get_contents($file->getPathname());
+        $translationData = json_decode($translationFile, true);
+        $translationData['language'] = substr($language, 0, 2);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            printf('ERROR: %s in language %s' . PHP_EOL, json_last_error_msg(), $language);
+            return 1;
+        }
+
+        printf('Generating blackout page "%s"…' . PHP_EOL, $filename);
+        $blackoutPage = $renderer->render($templateFile->getBasename(), $translationData);
+
+        file_put_contents($filename, $blackoutPage);
     }
-
-    printf('Generating blackout page "%s"…' . PHP_EOL, $filename);
-    $blackoutPage = $renderer->render('blackout.html.twig', $translationData);
-
-    file_put_contents($filename, $blackoutPage);
 }
 
 return 0;
